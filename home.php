70a8f7da81f41e6fafa425f7a8f60aa95f634725
SinisterPlot <tempted180@gmail.com>
2015-01-02 23:20:27 -0500

    new homepage

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 
 <head>
 
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="http://web.njit.edu/~fgl4/css/dist/css/bootstrap.css">
 		
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
@@ -15,7 +15,73 @@
 	
     <div class = "container">
 		  
-		<?php include 'navbar.php';?>
+		<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+      <img alt ="Brand" src = "http://web.njit.edu/~fgl4/shpelogoshit.png"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Opportunities <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Scholarships</a></li>
+             <li><a href="#">Internships</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Miscellaneous</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">E-Board <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Current E-Board</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Advisors</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Events <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Past Events</a></li>
+            <li><a href="#">Future Events</a></li>
+            <li><a href="#">Gala</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contact Form <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Gallery <span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About Us <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Mission & Vision</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">Testimonials</a></li>
+          </ul>
+        </li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+       <button type="button" class="btn btn-default navbar-btn">GBM Check-In</button>        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="alert alert-danger alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong><center> (In case there are some sudden updates to something a message will appear, like so) 
+    READ! There is no meeting today, it has been cancelled, pass it on!.</center></strong>
+</div>
     
 		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
@@ -66,19 +132,57 @@
 
     <div class ="row">
       <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+        
         <div class = "well">
           <div id ="newsthing">
             <h1><center>News</center></h1>
-            <hr>
-            <p><center>This is a news thing that all it does is displays some kind of news that
-                      is kinda or very relevant to SHPE This is a news thing that all it does 
-                      is displays some kind of news that is kinda or very relevant to SHPE 
-                      This is a news thing that all it does is displays some kind of news that
-                      is kinda or very relevant to SHPE This is a news thing that all it does 
-                      is displays some kind of news that is kinda or very relevant to SHPE</center></p>
-                      <div id = "containershit">
-            <img src="http://web.njit.edu/~fgl4/twittershit.PNG">
-          </div>
+            
+            
+           <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><strong>The Fisherman And The Jinni</strong></a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        IT hath reached me, O auspicious King, that there was a fisherman well stricken in years who had a wife and three children, and withal was of poor condition. Now it was his custom to cast his net every day four times, and no more. On a day he went forth about noontide to the seashore, where he laid down his basket and, tucking up his shirt and plunging into the water, made a cast with his net and waited till it settled to the bottom. Then he gathered the cords together and haled away at it, but found it weighty. And however much he drew it landward, he could not pull it up, so he carried the ends ashore and drove a stake into the ground and made the net fast to it. Then he stripped and dived into the water all about the net, and left not off working hard until he had brought it up.
+He rejoiced thereat and, donning his clothes, went to the net, when he found in it a dead jackass which had torn the meshes. Now when he saw it, he exclaimed in his grief, "There is no Majesty and there is no Might save in Allah the Glorious, the Great!" Then quoth he, "This is a strange manner of daily bread," and he began reciting in extempore verse:
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><strong>The Tale Of The Ensorceled Prince</strong></a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        KNOW then, O my lord, that whilom my sire was King of this city, and his name was Mahmud, entitled Lord of the Black Islands, and owner of what are now these four mountains. He ruled threescore and ten years, after which he went to the mercy of the Lord and I reigned as Sultan in his stead. I took to wife my cousin, the daughter of my paternal uncle, and she loved me with such abounding love that whenever I was absent she ate not and she drank not until she saw me again. She cohabited with me for five years till a certain day when she went forth to the hammam bath, and I bade the cook hasten to get ready all requisites for our supper. And I entered this palace and lay down on the bed where I was wont to sleep and bade two damsels to fan my face, one sitting by my head and the other at my feet.
+But I was troubled and made restless by my wife's absence and could not sleep, for although my eyes were closed, my mind and thoughts were wide-awake. Presently I heard the slave girl at my head say to her at my feet: "O Mas'udah, how miserable is our master and how wasted in his youth, and oh! the pity of his being so betrayed by our mistress, the accursed whore!" The other replied: "Yes indeed. Allah curse all faithless women and adulterous! But the like of our master, with his fair gifts, deserveth something better than this harlot who lieth abroad every night." Then quoth she who sat by my head, "Is our lord dumb or fit only for bubbling that he questioneth her not!" and quoth the other: "Fie on thee! Doth our lord know her ways, or doth she allow him his choice? Nay, more, doth she not drug every night the cup she giveth him to drink before sleeptime, and put bhang into it? So he sleepeth and wotteth not whither she goeth, nor what she doeth, but we know that after giving him the drugged wine, she donneth her richest raiment and perfumeth herself and then she fareth out from him to be away till break of day. Then she cometh to him and burneth a pastille under his nose and he awaketh from his death-like sleep." When I heard the slave girls' words, the light became black before my sight and I thought night would never fall.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><strong>The Sleeper And The Waker</strong></a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        IT hath reached me, O auspicious King, that there was once at Baghdad, in the caliphate of Harun al-Rashid, a man and a merchant who had a son Abu al-Hasan al-Khali'a by name. The merchant died leaving great store of wealth to his heir, who divided it into two equal parts, whereof he laid up one and spent of the other half. And he fell to companying with Persians and with the sons of the merchants, and he gave himself up to good drinking and good eating till all the wealth he had with him was wasted and wantoned. Whereupon he betook himself to his friends and comrades and cup companions and expounded to them his case, discovering to them the failure of that which was in his hand of wealth. But not one of them took heed of him or even deigned answer him.
+So he returned to his mother (and indeed his spirit was broken) and related to her that which had happened to him and what had befallen him from his friends, how they had neither shared with him nor requited him with speech. Quoth she: "O Abu al-Hasan, on this wise are the sons of this time: And thou have aught, they draw thee near to them, and if thou have naught, they put thee away from them." And she went on to condole with him, what while he bewailed himself and his tears flowed and he repeated these lines:
+
+      </div>
+    </div>
+  </div>
+</div>
+         
+            <img src="http://web.njit.edu/~fgl4/twittershit.PNG" class = "img center-block">
+         
 
 
 
@@ -92,7 +196,9 @@
 
 
       <div class= "col-lg-3 col-md-3 col-sm-3 col-xs-3">
+          
         <div class ="well">
+
           <div id = "dropavisit">
             <h3><center>Drop Us a Visit</center></h3>
             <hr>
@@ -125,7 +231,14 @@
           <div id = "Follow-us">
             <h3><center>Follow Us!</center></h3>
             <hr>
-            <img src="http://web.njit.edu/~fgl4/socialicons.png">
+            <div id="socialiconsbox" style ="padding-bottom: 10px; margin-top:0px" align = "center">
+<a class="facebook socialicons" href="https://www.facebook.com/" title="Facebook " target="_blank">f</a>
+<a class="twitter socialicons" href="https://twitter.com/" title="Twitter " target="_blank">t</a>
+
+</div>
+<div id="socialiconsbox" style ="padding-top: 12px; margin-top:0px" align = "center">
+  <a class="googleplus socialicons" href="https://plus.google.com/" title="Google+" target="_blank">g+</a>
+<a class="linkedin socialicons" href="https://linkedin.com/" title="Linkedin" target="_blank">in</a></div>
 
           </div>
         </div>
