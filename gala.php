5c6e80dcf1ad76cd9c07f7301bd2ed4043eef921
Mario DeLaPaz <mariocd10@gmail.com>
2015-01-11 14:23:52 -0500

    Created but not finished Gala Page

---
+++ gala.php
@@ -0,0 +1,103 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+
+  	<!-- =======================================Start Container ============================================-->
+    <div class = "container">
+  	<?php include 'navbar.php';?>
+
+    <div class="well">
+      <h1 class="title">Gala Banquet</h1>
+      <hr>
+
+      <h2><center>You're Invited!!</center></h2>
+      <p><center>Join us on April 19, 2015 at 6pm for the 23rd Annual Gala Banquet held at Iberia's Tavern
+        and Restaurant. We will honor the achievements of this past academic year's Executive Board and the 
+        corporate companies that have helped us through sponsorship, mentorship, and leading workshops. Enjoy
+        a night of networking, food, awards, and dance as we induct the new Executive Board for the next academic year.
+        Purchase your tickets today!
+        <br><br>
+        Interested in helping out? Join the Gala Banquet Committee today! Email njitshpe@gmail.com for more information.
+        </center></p>
+
+        <div class="container-fluid">
+          <h3 class="col-lg-5 col-lg-offset-1">Faculty/Staff</h3>
+          <h3 class="col-lg-5">Profressionals</h3>
+          <div class="row">
+            <div class="shadow-rectangle-box col-lg-4 col-lg-offset-1">
+              <div class="col-lg-5">
+                <h3>Individual</h3>
+                <br>
+                <h5>$60</h5>
+              </div>
+              <div class="right col-lg-5">
+                <h3>Table</h3>
+                <br>
+                <h5>$600</h5>
+              </div>
+            </div>
+            
+            <div class="shadow-rectangle-box col-lg-4">
+              <div class="col-lg-5">
+                <h3>Professional</h3>
+                <br>
+                <h5>$60</h5>
+              </div>
+              <div class="right col-lg-5">
+                <h3>Corporate Table</h3>
+                <br>
+                <h5>$600</h5>
+              </div>
+            </div>
+          </div>
+          <!-- =======================================End Row ============================================-->
+          <div class="row">
+            <h3 class="col-lg-5">Students/Guests</h3>
+            <h3 class="col-lg-5">SHPE Jr.</h3>
+          </div>
+          <div class="">
+            <div class="col-lg-5 col-lg-offset-1">
+
+              <div class="col-lg-5">
+                <h3>Early Bird</h3>
+                <br>
+                <h5>$60</h5>
+              </div>
+              <div class="right col-lg-5">
+                <h3>Standard</h3>
+                <br>
+                <h5>$600</h5>
+              </div>
+            </div>
+
+            <div class="col-lg-5">
+              <div class="col-lg-5">
+                <h3>Students</h3>
+                <br>
+                <h5>$60</h5>
+              </div>
+              <div class="right col-lg-5">
+                <h3>Parents</h3>
+                <br>
+                <h5>$600</h5>
+              </div>
+            </div>
+          </div>
+          <!-- =======================================End Row ============================================-->
+        </div>
+        <!-- =======================================End Container-Fluid ============================================-->
+    </div>
+    <!-- =======================================End Well ============================================-->
+  </div>
+  <!-- =======================================End Container ============================================-->
+  </body>
+
+</html>
