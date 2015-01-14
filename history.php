7d51526955c12d89268c26a52e102289541e1ca8
unknown <jgpereira23@gmail.com>
2015-01-13 23:59:58 -0500

    Commit

---
+++ history.php
@@ -0,0 +1,57 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+  	
+    <div class = "container">
+  	<!-- how am i going to change the active tab  -->
+  	  <?php include 'navbar.php';?>
+      
+
+
+    
+      <div class="well">
+        <div> 
+        <h1 class="titles"><center>History</center></h1>
+        <hr>
+        <p>The Society of Hispanic Professional Engineers (SHPE) was founded in Los Angeles, California, in 1974 by a group of engineers employed by the city of Los Angeles. 
+          Their objective was to form a national organization of professional engineers to serve as role models in the Hispanic community. The concept of Networking was the key basis for the organization. SHPE quickly established two student chapters to begin the network that would grow to encompass the nation as well as reach countries outside the United States. 
+          Today, SHPE enjoys a strong but independent network of professional and student chapters throughout the nation.
+        </p>
+        </div> 
+        <h1 class="titles">Our History</h1>
+        <p>
+          The NJIT Chapter of Hispanic Organization of Students in Technology/ Society of Hispanic Professional Engineers (HOST/SHPE) has a long history of dedication to its members and the community.
+        </p>
+        <h1 class="titles">OLA Years</h1>
+        <p>
+          In 1982, OLA (Organization Latino America) was created on the NJIT Campus. 
+          The OLA was dedicated to promoting higher educational level for the Hispanic student and motivating them to pursue a technological career.
+          OLA sponsored recreational, social, and academic activities that brought the culture,language, and customs of Hispanics into focus for everyone to enjoy.</p>
+        </p>
+        <h1 class="titles">HOST Years</h1>
+        <p>
+          However, in 1985, in an effort to embrace the math, engineering, and scientific nature of our university, OLA changed its name to HOST (Hispanic Organization of Students in Technology). As HOST, the organization not only dealt with Hispanic culture and history but also branched out into the technological world. Four years later, an entirely different Hispanic Organization name SHPE (Society of Hispanic Professional Engineers) brought a chapter to NJIT. 
+          SHPE, a national organizational formed to serve as a model in the community, has been around since 1974 nationally.
+        </p>
+        <h1 class="titles">HOST/SHPE Alliance</h1>
+        <p>
+          Even though HOST and SHPE shared the same goals and ideology, they were separate until 1991 when both organizations merged unto one and became known as HOST/SHPE. 
+          Since then, HOST/SHPE members have been involved in cultural events on and off campus, numerous community service activities, and all of SHPE's regional and national conferences. 
+          Moreover, HOST/SHPE has won a number of regional and national awards and it is widely recognized for educating, supporting, and instilling a sense of pride in aspiring students in the fields of engineering, technology, and science. 
+          As a matter of fact, HOST/SHPE received the Organization of the Year Award at the Newark College of Engineering Banquet on Friday, April 2,2004, alongside several congressional letters, national, and university awards.
+        </p>
+      </div>
+    </div>
+
+  </body>
+
+</html>
