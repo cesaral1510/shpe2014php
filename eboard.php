e9a031ead66860db53e67a20e396d2d4700802eb
Cesar Lazo <cal23@njit.edu>
2015-01-13 21:58:14 -0500

    updates to eboard

--- eboard.php
+++ eboard.php
@@ -1,101 +1,83 @@
 <!DOCTYPE>
 <html>
-
 <head>
+<?php
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_URL, 'http://web.njit.edu/~cal23/shpe2014/scripts/getEboard.php');
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $ch, CURLOPT_POST, 1 );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+    $output = curl_exec( $ch );
+    $errmsg = curl_error( $ch );
+    $info=json_decode($output,true);
+    curl_close( $ch );
 
+?>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-
-
-
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	
-    <div class = "container">
-		  
-		<?php include 'navbar.php';?>
-   
-
-    
-        <div class = "well">
-          <div id ="newsthing">
-
-
-            <h1><center>EBoard 2014-2015 </center></h1>
-            <hr>
-          <div class ="row">
-      
-          <div class="col-lg-12 board-container-left" >
-
-            <img src ="http://shpe.njit.edu/Images/EBoard/cesar.jpeg"> 
-                <div class="info_eboard">
-                    <p class="title_eboard">WEBMASTER</p>
-
-                    <p>Name: Cesar Lazo</p>
-                    <p>Major:Computer Science</p>
-                    <p>Email: cal23@njit.edu </p>
-                    <p>LinkedIn: Click here</p>
-                </div>
-      </div>
-    
-      <div class="space"></div>
-         <div class="col-lg-12 board-container-left" >
-
-            <img src ="http://shpe.njit.edu/Images/EBoard/cesar.jpeg"> 
-                <div class="info_eboard">
-                    <p class="title_eboard">WEBMASTER</p>
-
-                    <p>Name: Cesar Lazo</p>
-                    <p>Major:Computer Science</p>
-                    <p>Email: cal23@njit.edu </p>
-                    <p>LinkedIn: Click here</p>
-                </div>
-      
+	<div class = "container">
+		  <?php include 'navbar.php';?>
+          <div class = "well board">
+              <div id ="newsthing">
+                    <h1>EBOARD 2014-2015</h1>
+                    <hr>
+                      <div class ="row">
+      <?php
+          for($counter=0;$counter<count($info);$counter++){
+          echo ' <div class="col-lg-12 board-container-left" >';
+          echo '<img src ="'.$info[$counter][link].'">' ;
+          echo '   <div class="info_eboard">';
+          echo "<p class='title_eboard'><i>".$info[$counter][position]."</i></p>";
+          echo "<p> Name: ".$info[$counter][firstName]." ".$info[$counter][lastName]."</p>";
+          echo "<p> Major: ".$info[$counter][major]."</p>";
+          echo "<p> Email: ".$info[$counter][email]."</p>";
+          echo "<p> LinkedIn:</p>    </div> </div>";
+        }
+       ?>
+                    </div><!--end of row-->
+               </div><!--end of newsthing-->
+                <div id ="newsthing">
+                    <h1>Advisors</h1>
+                    <hr>
+                      <div class ="row">
      
+          
+           <div class="col-lg-12 board-container-left" >
+         <img src ="Eboard/Oquendo.jpeg"> 
+             <div class="info_eboard">
+          <p class='title_eboard'>ADVISOR </p>
+          <p> Name: Maria Oquendo</p>
+          <p>EOP Assistant Director for Counseling Services</p>
+          <p>Juniors</p>
+          <p>Email: maria.l.oquendo@njit.edu</p>
+            </div>
+             <div class="col-lg-12 board-container-left" >
+         <img src ="EBoard/Reyes.jpeg"> 
+             <div class="info_eboard">
+          <p class='title_eboard'>ADVISOR </p>
+          <p> Name: Nisha Reyes</p>
+          <p>SSSP Assistant Director</p>
+          <p>Email: nisha.reyes@njit.edu</p>
+            </div>
+           
 
-
-          </div>
-
-
-
-
-
-
-          </div>
-        </div>
-      </div>
-
-
-
+       
+       </div>
+        
       
+                    </div><!--end of row-->
+               </div><!--end of newsthing-->
+             </div><!--end of well-->
 
-       
-
-
-
-
-       
-
-
-        </div>
-      </div>
-    </div>
-
-    
+             
+            
 
 
 
-
-
-
-
-
-
-
-    </div>
-
+      </div><!--end of container-->
 </body>
 
 
