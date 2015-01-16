3a8424401ddf06f39dcb8fb051aba0010950afc3
unknown <jgpereira23@gmail.com>
2015-01-15 22:00:49 -0500

    Finished styling for testimonial page

---
+++ testimonials.php
@@ -0,0 +1,63 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+  	
+    <div class = "container">
+  	<!-- how am i going to change the active tab  -->
+  	  <?php include 'navbar.php';?>
+      
+
+
+    
+      <div class="well">
+        <div> 
+            <h1 class="titles"><center>Testimonials</center></h1>
+            <hr>
+            <div id="leftColumn">
+              <div class="testimonialHeader">
+                  <img class="testImage" src="EBoard/Vanessa.jpeg">
+                  <div class="testName" >
+                        Vanessa Granados
+                    </div>
+              </div>
+              <div class="testBody">
+                <img src="img/startingQuote.png" class="startingQuote">
+                <p>
+                  If you were to ask me when I was in high school who I expected to be in my final year of college, I couldn't have imagined it or painted a picture for you. I have learned, within the three years at NJIT, that the biggest failure of all, would have been the inability of not testing your limits, getting out of your comfort zone, and figuring out who you are, what you want, and how you will get there. Since my freshmen year, the SHPE Familia has been my biggest motivator and advocator of encouragement for professional advancement. Every year there’s an emergence of leaders within the organization and it’s a great feeling to know I've helped in that impact. As we continue to move toward climbing that ladder of success, SHPE reminds me that our truest accomplishment is to help that next generation of leaders make that first step. 
+
+                  As I come to complete my final year at NJIT, I'm proud to stand as the President of the SHPE chapter. At NJIT, it has been quiet a bumpy ride as a leader and as a student, but overall it has been a great learning experience. NJIT SHPE has made me realize that each experience or opportunity given play a vital role in personal development. To this day, I'm still making mistakes and learning from my own peers on how I can make them better. Great leaders aren't always the best at the time they start leading, but instead are the best when they reflect on how they finish.
+                </p>
+                <img src="img/endingQuote.png" class="endingQuote">
+              </div> 
+            </div>
+            <div id="rightColumn">
+              <div class="testimonialHeader">
+                  <img class="testImage" src="EBoard/kat.jpeg">
+                  <div class="testName">
+                        Katherine Dawson
+                    </div>
+              </div>
+              <div class="testBody">
+                <img src="img/startingQuote.png" class="startingQuote">
+                <p>
+                  Before coming to NJIT, I didn't have in mind to join clubs or organizations. However, once I met a few folks of SHPE, I did not hesitate to join the organization. When I first heard about SHPE, I couldn't stop listening to the information and feeling attracted by it; so I thought “You know what! It is just Hispanics. What could go wrong with that? I am Hispanic too, so it might just feel like being in a huge family.” Getting more involved with SHPE made me realized that it did not just feel like a family; they became my family! Before SHPE, I was hiding behind bushes covered with pessimism but today I am going on an optimistic journey towards a plethora of opportunities. There is always one person or a group of people who represent that changing point for one’s life; for me, SHPE is absolutely one of those.
+                </p>
+                <img src="img/endingQuote.png" class="endingQuote">
+              </div> 
+            </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+
+</html>
