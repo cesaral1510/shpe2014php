2128a4364064f50948f7fbd686b93671b900d8c9
Mario DeLaPaz <mariocd10@gmail.com>
2015-01-18 10:07:13 -0500

    Added curl call; PHP for loop

--- opportunities.php
+++ opportunities.php
@@ -1,3 +1,18 @@
+<?php 
+  //get the cURL resource
+  $ch = curl_init();
+  // Set Options
+  $ch = curl_init();
+  curl_setopt( $ch, CURLOPT_URL, 'https://web.njit.edu/~mcd24/shpe2014php/scripts/getOpportunities.php');
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+  curl_setopt( $ch, CURLOPT_POST, 1 );
+  curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+  $output = curl_exec( $ch );
+  $errmsg = curl_error( $ch );
+  $info=json_decode($output,true);
+  curl_close($ch);
+?>
+
 <!DOCTYPE>
 <html>
 
@@ -17,94 +32,31 @@
       <div class="well list-group" id="accordion" role="tablist" aria-multiselectable="true">
         <h1 class="title">Opportunities</h1>
         <hr>
-        <div class="opp-list">
-          <div class="panel-heading" role="tab" id="headingOne">
-            <h3 class="panel-title">
-              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-               <img src="img/port-authority.jpg" alt="Pic Not Found">
-              </a>
-            </h3>
-          </div>
-          <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-            <div class="panel-body opp-body">
-              <b><h4>Port Authority Full Time Positions</h4></b>
-              <p> The Port Authority is currently recruiting for its 2015 Engineering Associates Program 
-                The Engineering Associates Program is a unique program where our associates are hired as 
-                permanent employees and complete a rotation in either the design or construction discipline. 
-                Associates is this program get to experience various functions of engineering and how each department 
-                relates to one another. At the end of the first year of the two year program associates are placed 
-                into a role within one of our design or construction management departments. While in the program we 
-                give associates various training sessions to prepare them for a career in engineering. In addition 
-                to the various training sessions we also provide our associates with the opportunity to get their 
-                Masters degree and pay for all PE/FE testing. The Port Authority offers associates pre-paid PE testing 
-                Many of our engineering associates have gone on to become managers, directors and chiefs at the Port 
-                Authority.
-              </br></br>
 
-              <b> February 2, 2014 - January 24, 2015</b></p>
-              <button type="button" class="btn btn-success">Apply Here</button>
-            </div>
-          </div>
-        </div>
+        <?php
 
+        
+          for($counter=0;$counter<count($info);$counter++){
+            echo '<div class="opp-list">
+                  <div class="panel-heading" role="tab" id="heading'.$counter.'">
+                  <h3 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse'.$counter.'" aria-expanded="true" aria-controls="collapseOne">';
+            echo '<img src="#" alt="Pic Not Found">';
+            echo '</a>
+                  </h3>
+                  </div>
+                  <div id="collapse'.$counter.'" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="'.$counter.'">
+                  <div class="panel-body opp-body">';
+            echo '<b><h4>'.$info[$counter][title].'</h4></b>';
+            echo '<p>'.$info[$counter][description].'</br></br>';
+            echo '<b>'.$info[$counter][startDate].'-'.$info[$counter][endDate].'</b></p>';
+            echo '<a href="'.$info[$counter][link].'" class="btn btn-success" role="button">Apply Here</a>';
+            echo '</div></div></div>';
+          }
 
-        <div class="opp-list">
-          <div class="panel-heading" role="tab" id="headingTwo">
-            <h4 class="panel-title">
-              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <h3 class="title"><img src="img/intel.png"> Internships & Grad Openings Just for SHPE</h3>
-              </a>
-            </h4>
-          </div>
-          <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-            <div class="panel-body opp-body">
-              <b><h4>Internship Opportunities Just for SHPE</h4></b>
-              <p>Intel Corporation is actively accepting applications for their 2015 BS, MS and PhD 
-                Internships and New Grad openings and would like to invite students from the Society
-                of Hispanic Professional Engineers to apply. Create your own career. With opportunities
-                to change roles, teams and even locations, you have the power to shape your ideal career.
-                At Intel, we offer a broad range of BS, MS and PhD opportunities in the following technical
-                disciplines: </p>
-                <p>Applied Physics Chemical Engineering <br>Computer Engineering Construction Management<br>
-                Electrical Engineering Environmental or Safety Engineering <br>Industrial Engineering Materials
-                Science <br>Mechanical Engineering Math/Statistics
-                </br></br> 
-                <b> March 1, 2014 - July 31, 2015 </b>
-              </p>
-              <button type="button" class="btn btn-success">Apply Here</button>
-            </div>
-          </div>
-        </div>
-
-
-        <div class="opp-list">
-          <div class="panel-heading" role="tab" id="headingThree">
-            <h4 class="panel-title">
-              <a class="collapsed row" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-               <h3 class="title"><img src="img/prudential.png">Prudential's Peak Leadership</h3>
-              </a>
-            </h4>
-          </div>
-          <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-            <div class="panel-body opp-body">
-              <b><h4>Seeking NJIT Freshman and Sophmomore Students to Apply</h4></b>
-              <p> The Peak Leadership Conference is an interactive, three-day event that exposes college freshmen and
-               sophomores to Prudential's dynamic career paths and diverse, inclusive culture. -Students will have
-                the opportunity to learn more about Prudential career paths in IT/digital, asset management, finance,
-                marketing, accounting, and more. All travel and lodging expenses are covered by Prudential.
-                -Applications are being accepted from now through December 15th. Students can learn more at
-                <a href="www.prudential.com/peakleadership">Peak Leadership</a> <br>-Because we are committed to the power of an inclusive workplace,
-                students who are black, hispanic, Native American, LGBT, female, as well as veterans and students
-                with disabilities are encouraged to apply.
-                </br></br>
-                <b> February 1, 2014 - December 15, 2014</b>
-              </p>
-              <button type="button" class="btn btn-success">Apply Here</button>
-            </div>
-          </div>
-        </div>
-        <!-- =======================================End list items ============================================-->
-      </div>
+          echo '</div>';
+          echo '</div>';     
+        ?>    
       <!-- =======================================End Accordian ============================================-->
   </div>
   <!-- =======================================End Container ============================================-->
