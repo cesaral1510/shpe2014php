e731edfdc0f195de5d4c8958cf7b3937a8d87fcd
Cesar Lazo <cal23@njit.edu>
2015-01-21 23:23:42 -0500

    updates on events

---
+++ events.php
@@ -0,0 +1,44 @@
+<!DOCTYPE>
+<html>
+<head>
+
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/events.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class = "container">
+		  <?php include 'navbar.php';?>
+         <div class="well">
+            <p id="event-title"> test</p>
+
+           <img class="event-image" src="img/board.jpg">
+           <div class="event-info">
+           <p class="event-name">Dinner by the sea</p>
+           <b><p class="font-16 no-margin">Newark Port Tier </p></b>
+           <b><p class="font-16 "> Friday January 23rd 7pm-10pm</p> </b>
+          <p class="font-16">Modus graecis signiferumque pri id, per prima scriptorem ei. Ea iusto neglegentur vis. Duis delectus at his. Pri eu sale appareat detraxit. Cu vocibus nostrum detraxit mea, regione quaestio ut eam, no his omnis similique.
+
+ivendum laboramus, qui duis numquam dissentias eu. Dicam doming luptatum ei qui, in mei propriae qualisque. No nam rebum petentium, eu saepe ubique voluptatum ius. Omnes iisque qui te. Eu eam ridens insolens adversarium, mazim gloriatur vituperata vel an, ea vel utroque deseruisse.</p> 
+          
+           </div><!--end of event-info  -->
+           <div class="blue-bar">
+
+           </div>
+         </div><!--end of well  -->
+     
+                
+    
+           
+
+     
+            
+
+
+
+      </div><!--end of container-->
+</body>
+
+
+</html>
